baa2264b750a7a868054ae5a96f532402f7cb27b
Mahnaz Mohammadzamani <mahnaz.mzamani@gmail.com>
2021-11-13 16:57:38 +0100

    shinny app

---
+++ R/API1.R
@@ -0,0 +1,15 @@
+library(readxl)
+library(httr)
+library(RCurl)
+library(readxl)
+read_data <- function(url){
+  #url <- "https://data.val.se/val/val2014/statistik/index.html"
+  GET(url, write_disk(tf <- tempfile(fileext = ".xls")))
+  df1 <- read_excel(tf, 1L)
+  df1 <- df1[-1,]
+  names(df1) <- as.matrix(df1[1, ])
+  df1 <- df1[-1, ]
+  output <- list(df1, nrow(df1), ncol(df1))
+  return(output)
+}
+read_data("https://data.val.se/val/val2014/statistik/2014_landstingsval_per_kommun.xls")
